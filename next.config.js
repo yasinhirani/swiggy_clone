e883cbc6bd0634c85bd76233eb15f22534c470b8
Yasin Hirani <yasin.hirani@1rivet.local>
2023-10-05 13:46:10 +0530

    Fix : Added mongodb connect uri to env variables

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["media-assets.swiggy.com", "res.cloudinary.com"],
   },
+  env: {
+    MONGODB_CONNECT_URI: process.env.MONGODB_CONNECT_URI,
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
