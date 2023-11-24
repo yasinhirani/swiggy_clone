0ffeb1863dc223fae665e27b9431745c8dd621f4
Yasin Hirani <yasin.hirani@1rivet.local>
2023-11-24 15:13:46 +0530

    Fix : Added mapbox access token to env file

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ const nextConfig = {
     MONGODB_CONNECT_URI: process.env.MONGODB_CONNECT_URI,
     STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_SIGNATURE_SECRET: process.env.STRIPE_SIGNATURE_SECRET
+    STRIPE_SIGNATURE_SECRET: process.env.STRIPE_SIGNATURE_SECRET,
+    MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN
   },
   eslint: {
     dirs: ["app", "components", "core", "shared"]
